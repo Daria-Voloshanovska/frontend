1a0d6d289d4bd2919b403bc1eccb44089d9c5b57
Daria-Voloshanovska <voloshanovskadaria@gmail.com>
2025-04-05 20:35:28 +0200

    Lesson_18

---
+++ Lesson_18/beauty-tech/script.js
@@ -0,0 +1,128 @@
+const gridContainer = document.getElementById('grid-container');
+const loader = document.querySelector('.loader');
+const btnReset = document.querySelector('btn-reset');
+const amountInput = document.getElementById('limit');
+const form = document.getElementById('form-products');
+const burger = document.getElementById ('burger');
+const menu = document.getElementById('menu');
+const errorMessage = document.getElementById('error-message');
+
+
+let cartCount = 0;
+let cartItems = [];
+
+const cart = document.getElementById("cart");
+const cartPopup = document.getElementById("cart-popup");
+const cartItemsList = document.getElementById("cart-items");
+const clearCartBtn = document.getElementById("clear-cart");
+
+function updateCartCount() {
+  const countElement = document.getElementById("cart-count");
+  if (countElement) {
+    countElement.textContent = cartCount;
+  }
+}
+function updateCartList() {
+  cartItemsList.innerHTML = "";
+  cartItems.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    cartItemsList.appendChild(li);
+  });
+}
+
+
+if(burger && menu) {
+  burger.addEventListener("click", ()=>{
+  menu.classList.toggle("hide");
+  })
+}
+
+
+
+async function getProducts(limit = 30) {
+  gridContainer.innerHTML="";
+  loader.classList.remove('hide');
+  errorMessage.classList.add("hide");
+  errorMessage.textContent = "";
+
+  try {
+    const res = await fetch(`https://dummyjson.com/products?limit=${limit}`);
+    
+    if (!res.ok) throw new Error (`Status :${res.status} ${res.statusText || ''}`);
+  const data = await res.json()
+  
+   loader.classList.toggle('hide')
+   gridContainer.classList.remove('hide')
+
+   data.products.map(product => {
+      const section = document.createElement('section')
+
+      const h2 = document.createElement('h2')
+      h2.textContent = product.title
+
+      const img = document.createElement('img')
+      img.src = product.thumbnail;
+
+      const price = document.createElement('p');
+      price.textContent = `Price: $${product.price}`;
+
+     const btnAdd = document.createElement('button');
+      btnAdd.textContent = "Add to cart";
+      btnAdd.classList.add("btn-add");
+
+     btnAdd.addEventListener('click', () => {
+     cartCount++;
+     cartItems.push(product.title);
+     updateCartCount();
+     updateCartList();
+    });
+      section.append(h2, img, price, btnAdd);
+      gridContainer.append(section);
+    });
+
+    loader.classList.add('hide');
+    gridContainer.classList.remove('hide');
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    errorMessage.textContent = `Error: ${error.message}`;
+    errorMessage.classList.remove("hide");
+    loader.classList.add('hide');
+  }
+}
+   
+cart.addEventListener("click", () => {
+  cartPopup.classList.toggle("hide");
+});
+
+clearCartBtn.addEventListener("click", () => {
+  cartCount = 0;
+  cartItems = [];
+  updateCartCount();
+  updateCartList();
+  cartPopup.classList.add("hide");
+});
+
+form.addEventListener('submit', (event) =>{
+  event.preventDefault();
+  const limit = amountInput.value;
+  
+  if (limit < 1 || limit > 30) {
+      alert ('Please enter a number between 1 amd 30');
+      return;
+  }
+  getProducts(limit);
+  });
+
+setTimeout(getProducts, 2000)
+
+if(btnReset) {
+btnReset.addEventListener('click', () => {   
+loader.classList.toggle('hide')
+gridContainer.classList.toggle('hide')
+setTimeout(getProducts, 1500)
+});
+}
+
+
+  
